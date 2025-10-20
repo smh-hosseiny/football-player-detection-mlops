7a99db47d3abf9f319c939eb09fed6e163a396e7
Seyed M. Hosseini <49597221+smh-hosseiny@users.noreply.github.com>
2025-10-20 17:33:51 -0400

    Change protect_from_scale_in to false

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -75,7 +75,7 @@ resource "aws_autoscaling_group" "ecs" {
   health_check_type   = "ELB"
   health_check_grace_period = 300
 
-  protect_from_scale_in = true
+  protect_from_scale_in = false
 
   launch_template {
     id      = aws_launch_template.ecs.id
