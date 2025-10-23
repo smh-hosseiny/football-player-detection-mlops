ae8f878e41abe5b3a1540dcac5cb2726f611ac31
Seyed M. Hosseini <49597221+smh-hosseiny@users.noreply.github.com>
2025-10-23 10:07:07 -0400

    Change desired count of ECS service to 0
    Set desired count of ECS service to 0 for scaling.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -272,7 +272,7 @@ resource "aws_ecs_service" "app" {
   name            = "${var.app_name}-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 1  # Start with 1, scale-to-zero handles off-hours
+  desired_count   = 0
 
   capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.main.name
