a69c1a67951790e06b4ea42fb3edbad93f5b9b9f
Seyed Mohammad Hossein Hosseini <hosseiny290@gmail.com>
2026-02-08 11:17:49 -0500

    Fix CI: add explicit CPU PyTorch install to test jobs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -430,7 +430,7 @@ resource "aws_lb_listener" "https" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate_validation.app.certificate_arn
-  
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
